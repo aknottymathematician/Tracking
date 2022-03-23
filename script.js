5bc98ac3e2735cc62b4f34bc9b3b19151abd0aca
aknottymathematician <aknottymathematician@gmail.com>
2022-03-24 02:05:22 +0530

    new format

--- script.js
+++ script.js
@@ -1,89 +1,63 @@
-angular.module("myapp", [])
-        .controller("employeeController", function($scope) {
-            $scope.names = [
-                {problem:'2 Keys Keyboard',difficulty:'Medium',tag:'Greedy',comments:'uses greedy problem in sqrt time'},
-                {name:'Nikhil',lastname:'Kelkar',department:'QA',country:'India',designation:'TL QA',ratting:2},
-                {name:'Rahul',lastname:'Ghorpade',department:'QA',country:'India',designation:'QA',ratting:1},
-                {name:'Amol',lastname:'Bembde',department:'QA',country:'India',designation:'QA',ratting:3},
-                {name:'Nilesh',lastname:'Kamble',department:'QA',country:'India',designation:'QA',ratting:2},
-                {name:'Tamal',lastname:'Sen',department:'QA',country:'India',designation:'QA',ratting:4},
-                {name:'Sameer',lastname:'Shah',department:'QA',country:'India',designation:'Manager',ratting:2},
-                {name:'Arpan',lastname:'Mehta',department:'QA',country:'US',designation:'TL QA',ratting:3}
-            ];
-
-
-        });
-
 (function(document) {
-    'use strict';
+	'use strict';
 
-    var LightTableFilter = (function(Arr) {
+	var LightTableFilter = (function(Arr) {
 
-        var _input;
+		var _input;
+    var _select;
 
-        function _onInputEvent(e) {
-            _input = e.target;
-            var tables = document.getElementsByClassName(_input.getAttribute('data-table'));
-            Arr.forEach.call(tables, function(table) {
-                Arr.forEach.call(table.tBodies, function(tbody) {
-                    Arr.forEach.call(tbody.rows, _filter);
-                });
-            });
-        }
+		function _onInputEvent(e) {
+			_input = e.target;
+			var tables = document.getElementsByClassName(_input.getAttribute('data-table'));
+			Arr.forEach.call(tables, function(table) {
+				Arr.forEach.call(table.tBodies, function(tbody) {
+					Arr.forEach.call(tbody.rows, _filter);
+				});
+			});
+		}
+    
+		function _onSelectEvent(e) {
+			_select = e.target;
+			var tables = document.getElementsByClassName(_select.getAttribute('data-table'));
+			Arr.forEach.call(tables, function(table) {
+				Arr.forEach.call(table.tBodies, function(tbody) {
+					Arr.forEach.call(tbody.rows, _filterSelect);
+				});
+			});
+		}
 
-        function _filter(row) {
-            var text="";
+		function _filter(row) {
+      
+			var text = row.textContent.toLowerCase(), val = _input.value.toLowerCase();
+			row.style.display = text.indexOf(val) === -1 ? 'none' : 'table-row';
 
-            Arr.forEach.call(row.cells,function(cell,i){
-                if($('.advCheck')[i].checked){
+		}
+    
+		function _filterSelect(row) {
+     
+			var text_select = row.textContent.toLowerCase(), val_select = _select.options[_select.selectedIndex].value.toLowerCase();
+			row.style.display = text_select.indexOf(val_select) === -1 ? 'none' : 'table-row';
 
-                    if($($('.advCheck')[i]).attr("name")=="normal")
-                        text+=cell.textContent.toLowerCase();
-                    else
-                    {
-                        //console.log($(cell).find("option:selected").text());
-                    text+=$(cell).find("option:selected").val();
-                    }
-                }
-            });
-           var val = _input.value.toLowerCase();
-          //  var text = row.textContent.toLowerCase(), val = _input.value.toLowerCase();
-            row.style.display = text.indexOf(val) === -1 ? 'none' : 'table-row';
-        }
+		}
 
-        return {
-            init: function() {
-                var inputs = document.getElementsByClassName('light-table-filter');
-                Arr.forEach.call(inputs, function(input) {
-                    input.oninput = _onInputEvent;
-                });
-            }
-        };
-    })(Array.prototype);
+		return {
+			init: function() {
+				var inputs = document.getElementsByClassName('light-table-filter');
+				var selects = document.getElementsByClassName('select-table-filter');
+				Arr.forEach.call(inputs, function(input) {
+					input.oninput = _onInputEvent;
+				});
+				Arr.forEach.call(selects, function(select) {
+         select.onchange  = _onSelectEvent;
+				});
+			}
+		};
+	})(Array.prototype);
 
-
-
-    document.addEventListener('readystatechange', function() {
-        if (document.readyState === 'complete') {
-            LightTableFilter.init();
-        }
-    });
+	document.addEventListener('readystatechange', function() {
+		if (document.readyState === 'complete') {
+			LightTableFilter.init();
+		}
+	});
 
 })(document);
-
-
-$(function(){
-    var headers = $(".order-table thead th");
-
-     $(headers).each(function(i,cell) {
-        var celltext = cell.textContent;
-        var atrname = $(cell).attr("name")=="not"?"not":"normal";
-        if(celltext!=""){
-            $(".advFilter").append('<input name="'+atrname+'" id="'+celltext+'" class="advCheck" type="checkbox" checked/><label for="'+celltext+'">'+celltext+'</label>');
-        }
-        else
-        {
-            $(".advFilter").append('<div style="display:none"><input name="not" id="'+celltext+'" class="advCheck" type="checkbox" checked/><label for="'+celltext+'">'+celltext+'</label></div>');
-        }
-    });
-});
